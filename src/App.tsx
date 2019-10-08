58b20a4a50db554f0f46a6542ff4f55fabe3535c
tquinlan1992 <tquinlan1992@gmail.com>
2019-10-08 08:03:03 -0400

    clean up pricing and update background color on change

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,58 @@ import './App.css';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import { Pricing } from './components/Prices';
+import { Grid } from '@material-ui/core';
+import teikametricsLogo from './free_trial_assets/Teika Logos/TeikaLogo_H_black.svg';
 
 const App: React.FC = () => {
   return (
     <Provider store={store}>
-      <Pricing />
+      <Grid container justify="center">
+        <TeikametricLogo />
+        <SimplePricintStructure />
+        <SelectThePlanThatMatchesYourBusiness />
+        <Pricing />
+      </Grid>
     </Provider>
   );
 }
 
 export default App;
+
+const CenteredGrid: React.FunctionComponent = ({ children }) => {
+  return (
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justify="center"
+    >
+      {children}
+    </Grid>
+  )
+}
+
+function SimplePricintStructure() {
+  return (
+    <CenteredGrid>
+      <span style={{color:'#50C1DC'}}>OUR SIMPLE PRICING STRUCTURE</span>
+    </CenteredGrid>
+  );
+}
+
+function TeikametricLogo() {
+  return (
+    <CenteredGrid>
+      <img src={teikametricsLogo} alt='not found' />
+    </CenteredGrid>
+  );
+}
+
+function SelectThePlanThatMatchesYourBusiness() {
+  return (
+    <CenteredGrid>
+       Select the plan that matches your business
+    </CenteredGrid> 
+  );
+}
--- src/components/Prices.tsx
+++ src/components/Prices.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { actions, connectProps } from "../redux/store";
-import { Grid, Button, Switch } from '@material-ui/core';
+import { Grid, Button, Switch, makeStyles } from '@material-ui/core';
 import { PlanOptions, PlanRates } from '../redux/pricing/state';
 import { map } from 'lodash';
 import { plans } from './plans';
@@ -10,6 +10,36 @@ interface PlanInfo {
     monthlyPrice: string;
     annualPrice: string;
 }
+
+const annualBlue = '#50C1DC';
+const monthlyPurple = '#676DBC';
+
+function getPlanRateColor(selectedPlanRate: PlanRates) {
+    return selectedPlanRate === PlanRates.annual ? monthlyPurple : annualBlue;;
+}
+
+function colorSwitchStyles(color: string) {
+
+      return {
+        switchBase: {
+          color: 'white',
+          '&$checked + $track': {
+            backgroundColor: color,
+          }
+        },
+        checked: {},
+        track: {
+            backgroundColor: color
+        },
+      };
+}
+
+const switchColors = makeStyles({
+    annualBlue: colorSwitchStyles(annualBlue),
+    monthlyPurple: colorSwitchStyles(monthlyPurple)
+  });
+
+
 
 export const { Connected: Pricing } = connectProps(
     (state) => {
@@ -22,47 +52,76 @@ export const { Connected: Pricing } = connectProps(
     }
 )(({ selectPlan, selectedPlan, unselectPlan, selectedPlanRate, toggleSelectedPlaneRate }) => {
     if (selectedPlan === PlanOptions.noneSelected) {
+        const switchStyle = switchColors().annualBlue;
+        console.log('switchStyle', switchStyle);
         return (
             <>
-                <Switch
-                    checked={selectedPlanRate !== PlanRates.monthly}
-                    onChange={toggleSelectedPlaneRate}
-                    value={selectedPlanRate}
-                />
-                <Grid container justify="center" spacing={0}>
-                    {map(plans, (planInfo, value) => {
-                        const selectedPrice = getSelectedPrice( { selectedPlanRate, planInfo });
-                        return <PricingCard {...{title: planInfo.title, price: selectedPrice, key: value, onClick: () => selectPlan(Number(value) as PlanOptions)}} />
-                    })}
+                <Grid
+                    container
+                    direction="column"
+                    alignItems="center"
+                    justify="center"
+                    style={{ color: '#676DBC', height: '50px' }}
+                >
+                    <Switch
+                        checked={selectedPlanRate !== PlanRates.monthly}
+                        onChange={toggleSelectedPlaneRate}
+                        value={selectedPlanRate}
+                        className={monthlyPurple}
+                    />
                 </Grid>
+                {map(plans, (planInfo, value) => {
+                    const selectedPrice = getSelectedPrice({ selectedPlanRate, planInfo });
+                    return <PricingCard selectedPlanRate={selectedPlanRate} {...{ title: planInfo.title, price: selectedPrice, key: value, onClick: () => selectPlan(Number(value) as PlanOptions) }} />
+                })}
             </>
         )
     } else {
         const planInfo = plans[selectedPlan];
-        const selectedPrice = getSelectedPrice( { selectedPlanRate, planInfo });
+        const selectedPrice = getSelectedPrice({ selectedPlanRate, planInfo });
         return (
-            <Grid container justify="center" spacing={0}>
-                <Button onClick={unselectPlan}>Go Back</Button>
-                <PricingCard {...{title: planInfo.title, price: selectedPrice}} key={selectedPlan} />
-            </Grid>
+            <>
+                <Grid
+                    container
+                    direction="column"
+                    alignItems="center"
+                    justify="center"
+                    style={{ color: '#676DBC', height: '50px' }}
+                >
+                </Grid>
+
+                <PricingCard selectedPlanRate={selectedPlanRate} {...{ title: planInfo.title, price: selectedPrice }} hideSelectPlan={true} key={selectedPlan} />
+                <Grid
+                    container
+                    direction="column"
+                    alignItems="center"
+                    justify="center"
+                    style={{ color: '#676DBC' }}
+                >
+                    <Button onClick={unselectPlan}>Go Back</Button>
+                </Grid>
+            </>
         )
     }
 });
 
-function getSelectedPrice({selectedPlanRate, planInfo}: {selectedPlanRate: PlanRates, planInfo: PlanInfo}) {
+function getSelectedPrice({ selectedPlanRate, planInfo }: { selectedPlanRate: PlanRates, planInfo: PlanInfo }) {
     return selectedPlanRate === PlanRates.monthly ? planInfo.monthlyPrice : planInfo.annualPrice;
 }
 
-const PricingCard: React.FunctionComponent<{ title: string; onClick?: () => void; price: string; }> = ({ title, onClick, price }) => {
+const PricingCard: React.FunctionComponent<{ title: string; onClick?: () => void; price: string; hideSelectPlan?: boolean; selectedPlanRate: PlanRates }> = ({ title, onClick, price, hideSelectPlan = false, selectedPlanRate }) => {
+
+    const SelectPlanButton = () => hideSelectPlan ? null : <Button onClick={onClick}>Select Plan</Button>;
+    const backgroundColor = getPlanRateColor(selectedPlanRate);
     return (
-        <Grid item>
-            <Grid item xs={12} style={{ backgroundColor: '#676DBC', height: '55px', color: 'white' }}>
+        <Grid item spacing={8}>
+            <Grid item xs={12} style={{ backgroundColor, height: '55px', color: 'white' }}>
                 {title}
             </Grid>
             <Grid item justify="center" style={{ borderColor: '#D8D8D8', borderStyle: 'solid', borderWidth: '1px', height: '200px', width: '200px' }}>
                 {`$${price}`}
                 <br />
-                <Button onClick={onClick}>Select Plan</Button>
+                <SelectPlanButton />
             </Grid>
         </Grid>
     );
